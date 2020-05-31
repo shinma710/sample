e83fe2e1410196c40273a7a778992bbdba6b4178
shinma710 <shinmaaoisora@gmail.com>
2020-05-31 17:37:23 +0900

    追加

--- sample_app/urls.py
+++ sample_app/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 app_name = 'sample_app'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('info/', views.info, name='info'),
-    path('question/', views.question, name='question'),
+    path('', views.IndexView.as_view(), name='index'),
+    # path('about/', views.about, name='about'),
+    # path('info/', views.info, name='info'),
+    path('sample_app/<int:pk>/', views.DetailView.as_view(), name='question'),
+    path('sample_app/<int:pk>/plot', views.get_svg, name='plot'),
 ]
--- sample_app/views.py
+++ sample_app/views.py
@@ -1,29 +1,54 @@
-"""This is a test program."""
-from django.shortcuts import render
-from .forms import UserForm
+from django.http import HttpResponse
+from django.views import generic
+from .models import Location, WeatherData
+from django.contrib.auth.mixins import LoginRequiredMixin
+import io
+import matplotlib.pyplot as pp
 
 
-def index(request):
-    context = {
-        'name' : 'iima',
-    }
-    return render(request, 'sample_app/index.html', context)
 
-def about(request):
-    """/about アバウトページ"""
-    return render(request, 'sample_app/about.html')
+# def index(request):
+#     context = {
+#         'name' : 'iima',
+#     }
+#     return render(request, 'sample_app/index.html', context)
 
-def info(request):
-    """/info インフォページ"""
-    return render(request, 'sample_app/info.html')
+# def about(request):
+#     """/about アバウトページ"""
+#     return render(request, 'sample_app/about.html')
 
-def question(request):
-    params = {'name': '', 'email': '', 'form': None}
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        params['name'] = request.POST['name']
-        params['email'] = request.POST['email']
-        params['form'] = form
-    else:
-        params['form'] = UserForm()
-    return render(request, 'sample_app/question.html', params)
+# def info(request):
+#     """/info インフォページ"""
+#     return render(request, 'sample_app/info.html')
+
+class IndexView(LoginRequiredMixin, generic.ListView):
+    model = Location
+    paginate_by = 5
+    ordering = ['-updated_at']
+    template_name = 'sample_app/index.html'
+
+class DetailView(generic.DetailView):
+    model = Location
+    template_name = 'sample_app/question.html'
+
+
+def setPlt(pk):
+    weather_data = WeatherData.objects.select_related('location').filter(location_id=pk)
+    x = [data.data_datetime for data in weather_data]
+    y1 = [data.temperature for data in weather_data] # 気温
+    y2 = [data.humidity for data in weather_data]  # 湿度
+    pp.plot(x, y1, x, y2)
+
+def pltToSvg():
+    buf = io.BytesIO()
+    pp.savefig(buf, format='svg', bbox_inches='tight')
+    s = buf.getvalue()
+    buf.close()
+    return s
+
+def get_svg(request, pk):
+    setPlt(pk)
+    svg = pltToSvg()  # convert plot to SVG
+    pp.cla()  # clean up plt so it can be re-used
+    response = HttpResponse(svg, content_type='image/svg+xml')
+    return response
